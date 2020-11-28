12d06934bba979adab48941d11f9219f01378b28
PandelisT <pandeli@nerdypandy.com>
2020-11-28 11:01:55 +1100

    Added user model, schema and controller with settings and commands

---
+++ src/models/User.py
@@ -0,0 +1,14 @@
+from main import db
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(), nullable=False, unique=True)
+    password = db.Column(db.String(), nullable=False)
+
+    account_id = db.relationship(Account, backref="user")
+
+    def __repr__(self):
+        return f"<User {self.email}>"
